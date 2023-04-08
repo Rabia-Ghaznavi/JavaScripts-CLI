cee4851b2919270484830fa23929457c22a6382a
Rabia-Ghaznavi <ghaznavi.rabia@gmail.com>
2023-04-08 09:20:29 -0700

    FEAT:Initialize Task

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+// alert('by')
+// document.write("tata");
+// var zakaat ="Enter";
+// var yourRange="Enter Range";
+// console.log( zakaat+"   " +yourRange);
+// 
+// -------------------------------------------TASK 1-------------------------------------
+// 
+let zakatPercentage = 0.025; //camelCase
+let  UserInput= +prompt("Enter Your Amount to see ZakatAmount")   //PascalCase
+let result_is=(zakatPercentage*UserInput);  //snake_case
+alert("Your Zakat value is" +result_is);
+
