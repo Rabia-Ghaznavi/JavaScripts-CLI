08ce29d5dbe0b7af97e5359dbc249d4189f94215
Rabia-Ghaznavi <ghaznavi.rabia@gmail.com>
2023-04-08 19:21:06 -0700

    FEAT:PRACTICE

---
+++ scripts/practice.js
@@ -0,0 +1,35 @@
+// ==================================PRACTICE----------------------------------//
+
+//---------------------Making Variable---------------------------------//
+// alert("HELOO");
+// let num1=+prompt("Enter num1:");
+// let num2=+prompt("Enter num2:");
+// let num3=num1+num2;
+// let num4=prompt("Your ans is "+ num3);
+// alert(BYYY);
+// --------------------------------------------------------//
+
+// -----------------------bUILT-IN-FUNCTION------------------//
+
+// alert("hELLO");
+// document.write("tata");
+// prompt("Saalam  ");
+// console.log("HIII");
+//--------------------------------------------------------------//
+
+// ---------------------Using Variable Cases(LEGAL)-----------------------//
+
+//practice="do practice";     //variable for string 
+// camelCase=12;              //variable for number
+// snake_case=13;
+// PascalCase=camelCase*snake_case;
+// alert("12 *13 ="+ PascalCase);
+// ------------------------------------------------------------------------------//
+//--------------------------MATH EXPRESSION--------------------------------------//
+// let num = 2+2;
+// alert(num);
+// let num1= num*2;
+// document.write(num1);
+
+
+
